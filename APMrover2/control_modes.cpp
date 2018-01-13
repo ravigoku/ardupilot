7be51942367aaec2abeeb6a95aa74cea4a58f429
ravigoku <ravi.bauer@rutgers.edu>
2018-01-12 22:30:04 -0500

    edited control_modes file and parameters file

--- APMrover2/control_modes.cpp
+++ APMrover2/control_modes.cpp
@@ -33,6 +33,9 @@ Mode *Rover::mode_from_mode_num(const enum mode num)
     case INITIALISING:
         ret = &mode_initializing;
         break;
+    case SPECIAL:
+        ret = &mode_special;
+        break;
     default:
         break;
     }
